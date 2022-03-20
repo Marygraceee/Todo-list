4fd5389e43d89ad0131b7c8f7a1373fbda97e6cc
Marygrace <ma.grasso1999@gmail.com>
2022-03-20 22:42:06 +0100

    almost added every functionality

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let projectNameInput = document.getElementById("projectname")
 projectNameInput.value = ""
 let addProjectBtn = document.getElementById("addprojectbtn")
 let deleteProjectBtn = document.getElementById("deleteprojectbtn")
+
 let projects = Array.from(projectsContainer.children)
 let tabs = Array.from(tabsContainer.children)
 
@@ -26,16 +27,18 @@ function selectProject(){
         tab.classList.add("disabled")
     })
             tabs.forEach((tab)=>{
-                if (project.classList[0] === tab.classList[0]){
-                    project.className = project.className + " active"
+                if (project.classList[0] === tab.classList[0] && (projects.indexOf(project)) === (tabs.indexOf(tab))){
+                    project.classList.add("active")
                     tab.classList.add("active")
                     tab.classList.remove("disabled")
-                    console.log(project.classList)
-                    console.log(tab.classList)
+                    console.log(projects.indexOf(project))
+                    console.log(tabs.indexOf(tab))
+                    
                 }
             })
         })
     })
+    
 }
 
 
@@ -43,35 +46,57 @@ function selectProject(){
 
 // 2. adding the ability to create projects and their related tab
 
+addProjectBtn.addEventListener("click", addProject)
+
 function addProject(){ 
-    addProjectBtn.addEventListener("click", ()=>{
-        // Create the new project
-        let newProject = document.createElement("div")
-        newProject.textContent = projectNameInput.value 
-        newProject.classList.add(`${projectNameInput.value}`)
-        // Create the new tab
-        let newTab = document.createElement("div")
-        // creating and appending the button and the input to the tab
-        let newTaskButton = document.createElement("button")
-        newTaskButton.textContent = "Add task"
-        newTaskButton.classList.add("addTaskBtn")
-        let newTaskInput = document.createElement("input")
-        newTaskInput.classList.add("addTaskInput")
-        newTab.append(newTaskButton, newTaskInput)
-        // -----------------------------------------------------------
-        newTab.classList.add(`${projectNameInput.value}`, "disabled")
-        // adding the new project and the new tab to the containers
-        tabsContainer.append(newTab)
-        projectsContainer.appendChild(newProject)
-        // adding the new project and tab to the arrays, in order to make the "selectProject" function work
-       projects = Array.from(projectsContainer.children)
-       tabs = Array.from(tabsContainer.children)
-       projectNameInput.value = ""
-       selectProject()
-    })    
+    
+   
+       
+        
+    
+ // Create the new project
+        
+ let newProject = document.createElement("div")
+ newProject.textContent = projectNameInput.value 
+ newProject.className = `${projectNameInput.value}`
+ if (projectNameInput.value === ""){
+     alert("Give a name to the project!")
+ }
+ 
+ else {
+// Create the new tab
+let newTab = document.createElement("div")
+// creating and appending the button, the input and the delete button to the tab
+let newTask = document.createElement("div")
+let newTaskButton = document.createElement("button")
+newTaskButton.textContent = "Add task"
+newTaskButton.classList.add("addTaskBtn")
+let newTaskInput = document.createElement("input")
+newTaskInput.classList.add("addTaskInput")
+
+newTask.append(newTaskButton, newTaskInput)
+newTab.append(newTask)
+// -----------------------------------------------------------
+
+newTab.className = `${projectNameInput.value} disabled` 
+// adding the new project and the new tab to the containers
+tabsContainer.append(newTab)
+projectsContainer.appendChild(newProject)
+// adding the new project and tab to the arrays, in order to make the "selectProject" function work
+projects = Array.from(projectsContainer.children)
+tabs = Array.from(tabsContainer.children)
+projectNameInput.value = ""
+selectProject()
+addTask()
+
+ }
+        
+       
+       
+       
 }
 
-addProject()
+
 
 
 // 3. When you have an active project and you click the delete button, it deletes the project and its tab
@@ -82,23 +107,85 @@ function deleteProject(){
    
     projects.forEach((project)=>{
         if (project.classList.contains("active")){
-            console.log(project)
+            
             project.remove()
             projects = Array.from(projectsContainer.children)
-            console.log(projectsContainer)
-            console.log(projects)
+            
         }  
         })
 
         tabs.forEach((tab)=>{
             if (tab.classList.contains("active")){
-                console.log(tab)
+                
                 tab.remove()
                 tabs = Array.from(tabsContainer.children)
-                console.log(tabsContainer)
-                console.log(tab)
+                
             }
             })
-       
+}
+    // 4. within a specific tab, you can add tasks to it
+
+function addTask(){
+    let newTaskBtn = document.querySelectorAll(".addTaskBtn")
+    
+
+    for (let i = newTaskBtn.length - 1; i < newTaskBtn.length; i++){
+        newTaskBtn[i].addEventListener("click", ()=>{
+            if (newTaskBtn[i].nextSibling.value !== ""){
+                
+                let newTask = document.createElement("div")
+                newTask.textContent = newTaskBtn[i].nextSibling.value
+                newTask.className = `${i}`
+                let deleteTaskBtn = document.createElement("button")
+                deleteTaskBtn.classList.add("deleteTaskBtn")
+                deleteTaskBtn.textContent = "Delete"
+                newTask.append(deleteTaskBtn)
+                newTaskBtn[i].parentElement.parentElement.append(newTask)
+                newTaskBtn[i].nextSibling.value = ""
+                deleteTask()
+            }
+            else {
+                alert("Insert a task description, you can't have an empty task!")
+            }
+        
+            
+        })
+    }
     
 }
+ 
+
+/*newTaskBtn.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        console.log(button.nextSibling.value)
+        
+    }) */
+
+
+   /* for (let i = newTaskBtn.length - 1; i < newTaskBtn.length; i++){
+        newTaskBtn[i].addEventListener("click", ()=>{
+            console.log(newTaskBtn[i].nextSibling.value)
+        })
+    }*/
+
+
+
+
+    // 5. within a specific tab, you can delete a single task.
+
+    function deleteTask(){
+        let deleteTaskBtn = document.querySelectorAll(".deleteTaskBtn")
+    
+
+    for (let i = deleteTaskBtn.length - 1; i < deleteTaskBtn.length; i++){
+        deleteTaskBtn[i].addEventListener("click", ()=>{
+            
+                console.log(deleteTaskBtn[i])
+                console.log(deleteTaskBtn[i].parentElement)
+                deleteTaskBtn[i].parentElement.remove(deleteTaskBtn[i])
+        })
+    }
+}
+        
+            
+   
